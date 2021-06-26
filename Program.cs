3b695a3649f4df0493784e0c92acba4e6cef947c
MatheusAlmeidaNascimento <86276081+MatheusAlmeidaNascimento@users.noreply.github.com>
2021-06-25 23:11:24 -0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Exercicio_7
+{
+    class Program
+    {
+         static void Main(string[] args)
+        {
+            double[] numeros = null;
+            int quantidade = 15;
+            numeros = new double[quantidade];
+
+            for (var i = 0; i < quantidade; i++)
+            {
+                Console.Write($"Digite o número {i + 1}: ");
+                numeros[i] = double.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine("\nNúmeros digitados em ordem contrária");
+
+            foreach (var item in numeros)
+            {
+                Console.Write(numeros[quantidade - 1] + " ");
+                quantidade = quantidade - 1;
+            }
+        }
+    }
+}
